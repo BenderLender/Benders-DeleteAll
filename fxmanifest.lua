caf9e880a1196e3637ad7075b69ca9449bb110fe
Bender's Development <134189873+BenderLender@users.noreply.github.com>
2023-07-25 17:13:07 -0500

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,13 @@
+fx_version 'bodacious'
+game 'gta5'
+
+server_script {
+    'config.lua',
+    'server/sv.lua'
+}
+client_scripts {
+    'config.lua',
+    'client/cl.lua',
+    'client/entityiter.lua'
+}
+
